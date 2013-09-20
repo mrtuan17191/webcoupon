c46b247dfa343c67e2a7aa4f20469fc68673a803
Tuan <mr.tuan171@gmail.com>
2013-09-20 22:49:34 +0700

    Initailize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webcoupon::Application.config.secret_key_base = '15cfefa243bca1db256207e3b5f5332e4dacec9531921a143d8b5e5a155098cf2e874fc1711a319004e5f348ecca3f7c5f2e45416242c47dc2cc04f2d599d62d'
